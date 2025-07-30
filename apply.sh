5e33fe4718e9251d4653a0513367f81bb49915a3
Jan Richter <jrichter@redhat.com>
2025-07-30 11:52:38 +0200

    use official lightspeed images

--- apply.sh
+++ apply.sh
@@ -13,7 +13,8 @@ export RHDH_URL="https://rhdh-${RHDH_NAMESPACE}.$(oc get ingress.config.openshif
 export GH_PRIVATE_KEY_FILE="${GH_PRIVATE_KEY_FILE:-key.pem}"
 export GH_PRIVATE_KEY="$(cat $GH_PRIVATE_KEY_FILE | base64 -w 0)"
 export GH_PLACEHOLDER="$(echo -n PLACEHOLDER | base64 -w 0)"
-export LIGHTSPEED_IMAGE_TAG="${LIGHTSPEED_IMAGE_TAG:-quay.io/karthik_jk/lightspeed:latest}"
+export LIGHTSPEED_FRONTEND_TAG="${LIGHTSPEED_FRONTEND_TAG:-ghcr.io/redhat-developer/rhdh-plugin-export-overlays/red-hat-developer-hub-backstage-plugin-lightspeed:bs_1.39.1__0.5.7}"
+export LIGHTSPEED_BACKEND_TAG="${LIGHTSPEED_BACKEND_TAG:-ghcr.io/redhat-developer/rhdh-plugin-export-overlays/red-hat-developer-hub-backstage-plugin-lightspeed-backend:bs_1.39.1__0.5.7}"
 export INSIGHTS_IMAGE_TAG="${INSIGHTS_IMAGE_TAG:-quay.io/jrichter/adoption-insights:test}"
 
 if oc project ${RHDH_NAMESPACE} > /dev/null 2>&1; then
@@ -35,8 +36,6 @@ else
   binary=./kustomize
 fi
 
-$binary build $mode | envsubst '${RHDH_NAMESPACE},${RHDH_URL},${LIGHTSPEED_IMAGE_TAG},${INSIGHTS_IMAGE_TAG}' | sed "s/$GH_PLACEHOLDER/$GH_PRIVATE_KEY/" | oc apply -f -
+$binary build $mode | envsubst '${RHDH_NAMESPACE},${RHDH_URL},${LIGHTSPEED_FRONTEND_TAG},${LIGHTSPEED_BACKEND_TAG},${INSIGHTS_IMAGE_TAG}' | sed "s/$GH_PLACEHOLDER/$GH_PRIVATE_KEY/" | oc apply -f -
 
-oc rollout status deployment rhdh
-
-echo "RHDH running at: ${RHDH_URL}"
+oc rollout status deployment rhdh && echo "RHDH running at: ${RHDH_URL}"
